7c5533d61763edd8f1c046ed92b4c5fd82274c02
Gaetan Renaudeau <gre@zenexity.com>
2012-12-19 21:26:08 +0100

    Add stderr logs (requires by some cmd like curl) + add find example + add curl example

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,27 +4,32 @@ import play.api._
 import play.api.mvc._
 
 
-import java.io.{InputStream, OutputStream}
+import java.io._
 import scala.sys.process.{ Process, ProcessIO }
 import play.api.libs.iteratee._
 
 case class CLI (cmd: String) {
+
+  val logger = play.api.Logger("CLI")
 
   def enumerate: Enumerator[Array[Byte]] = 
     Enumerator.flatten[Array[Byte]] {
       import scala.concurrent.ExecutionContext.Implicits.global
       val process = Process(cmd)
       val promiseOfInputStream = concurrent.promise[InputStream]()
-      try {
-        process run new ProcessIO(
-          (stdin: OutputStream) => (), 
-          (stdout: InputStream) => promiseOfInputStream.success(stdout),
-          (stderr: InputStream) => ()
-        );
-      } catch {
-        case e: Throwable => promiseOfInputStream.failure(e)
-      }
-
+      process run new ProcessIO(
+        _.close(),
+        (stdout: InputStream) => promiseOfInputStream.success(stdout),
+        (stderr: InputStream) => {
+          val br = new java.io.BufferedReader(new InputStreamReader(stderr))
+          var read = br.readLine()
+          while(read != null) {
+            logger.warn(read)
+            read = br.readLine()
+          }
+          stderr.close()
+        }
+      );
       promiseOfInputStream.future.map { cmdout =>
         Enumerator.fromStream(cmdout)
       }
@@ -34,9 +39,19 @@ case class CLI (cmd: String) {
 }
 
 object Application extends Controller {
+
+  def index = find
   
-  def index = Action {
-    Ok.stream(CLI("ls").enumerate >>> Enumerator.eof)
+  def find = Action {
+    Ok.stream(CLI("find .").enumerate >>> Enumerator.eof).withHeaders(
+      CONTENT_TYPE -> "text/plain"
+    )
+  }
+  
+  def curl = Action {
+    Ok.stream(CLI("curl http://blog.greweb.fr/").enumerate >>> Enumerator.eof).withHeaders(
+      CONTENT_TYPE -> "text/html"
+    )
   }
   
 }
