7f936ecd8b90eeebbaba1aa0586a6dbfde5593ae
Gaetan Renaudeau <gre@zenexity.com>
2012-12-21 09:34:39 +0100

    Improve API

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,16 +4,18 @@ import play.api._
 import play.api.mvc._
 
 import java.io._
-import scala.sys.process.{ Process, ProcessIO }
+import scala.sys.process.{ ProcessIO, ProcessBuilder }
 import play.api.libs.iteratee._
 
-case class CLI (cmd: String) {
+object CLI {
 
   val logger = play.api.Logger("CLI")
 
+  /*
   def process = {
     Process(cmd)
   }
+  */
 
   def logstderr (stderr: InputStream) = {
     val br = new java.io.BufferedReader(new InputStreamReader(stderr))
@@ -30,10 +32,10 @@ case class CLI (cmd: String) {
    * (nothing is sent to the CLI input)
    *
    * @example {{{
-   CLI("find .").enumerate
+   CLI.enumerate("find .")
    * }}}
    */
-  def enumerate: Enumerator[Array[Byte]] = 
+  def enumerate (process: ProcessBuilder): Enumerator[Array[Byte]] = 
     Enumerator.flatten[Array[Byte]] {
       import scala.concurrent.ExecutionContext.Implicits.global
       val promiseOfInputStream = concurrent.promise[InputStream]()
@@ -52,10 +54,10 @@ case class CLI (cmd: String) {
    *
    * @example {{{
      // Add an echo to an ogg audio stream.
-     oggStream &> CLI("sox -t ogg - -t ogg - echo 0.5 0.7 60 1")
+     oggStream &> CLI.pipe("sox -t ogg - -t ogg - echo 0.5 0.7 60 1")
    * }}}
    */
-  def pipe: Enumeratee[Array[Byte], Array[Byte]] = {
+  def pipe (process: ProcessBuilder): Enumeratee[Array[Byte], Array[Byte]] = {
     Enumeratee.map[Array[Byte]] { bytes => bytes } // FIXME
     /*
     import scala.concurrent.ExecutionContext.Implicits.global
@@ -87,10 +89,11 @@ case class CLI (cmd: String) {
    * to push in the CLI (regardless of the CLI output).
    *
    * @example {{{
-     enumerator( CLI("aSideEffectCommand").consume )
+     val consumer = CLI.consume("aSideEffectCommand")
+     anEnumerator(consumer)
    * }}}
    */
-  def consume: Iteratee[Array[Byte], Unit] = {
+  def consume (process: ProcessBuilder): Iteratee[Array[Byte], Unit] = {
     Iteratee.flatten[Array[Byte], Unit] {
       import scala.concurrent.ExecutionContext.Implicits.global
       val promiseOfOutputStream = concurrent.promise[OutputStream]()
@@ -126,7 +129,7 @@ object Application extends Controller {
         Iteratee.foreach[Array[Byte]](channel.push(_))
       }
     }, () => ())
-    val addEcho = CLI("sox -t ogg - -t ogg - echo 0.5 0.7 60 1").pipe
+    val addEcho = CLI.pipe("sox -t ogg - -t ogg - echo 0.5 0.7 60 1")
     Ok.stream(radio &> addEcho &> Concurrent.dropInputIfNotReady(50))
       .withHeaders(
         CONTENT_TYPE -> "audio/ogg"
@@ -135,21 +138,21 @@ object Application extends Controller {
 
   // List all files in this Play project
   def find = Action {
-    Ok.stream(CLI("find .").enumerate >>> Enumerator.eof).withHeaders(
+    Ok.stream(CLI.enumerate("find .") >>> Enumerator.eof).withHeaders(
       CONTENT_TYPE -> "text/plain"
     )
   }
   
   // Retrieve a webpage and display it (of course, this is just for the demo, I won't use curl, prefer using WS)
   def curl = Action {
-    Ok.stream(CLI("curl -s http://blog.greweb.fr/").enumerate >>> Enumerator.eof).withHeaders(
+    Ok.stream(CLI.enumerate("curl -s http://blog.greweb.fr/") >>> Enumerator.eof).withHeaders(
       CONTENT_TYPE -> "text/html"
     )
   }
 
   // consume a ogg sound, add an echo effect and store in a /tmp/out.ogg file
   def audioEchoEffectGenerate = Action {
-    val consumer = CLI("sox -t ogg - /tmp/out.ogg echo 0.5 0.7 60 1").consume
+    val consumer = CLI.consume("sox -t ogg - /tmp/out.ogg echo 0.5 0.7 60 1")
     Enumerator.fromFile(Play.getFile("conf/exemple.ogg"))(consumer)
     Ok("'/tmp/out.ogg' file has been generated.")
   }
