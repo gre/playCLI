cf47944732f3b6ddf271e6efa4f10682ff934586
Gaëtan Renaudeau <gre@zenexity.com>
2012-12-28 20:05:46 +0100

    Make examples more readable

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,73 +17,65 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
+  // Pipe examples
+  val grep = (q: String) => CLI.pipe(Seq("grep", q), 64)
+  val addEchoToOgg = CLI.pipe("sox -t ogg - -t ogg - echo 0.5 0.7 60 1")
+  val scaleVideoHalf = CLI.pipe("ffmpeg -i pipe:0 -vf scale=iw/2:-1 -f avi pipe:1")
+
+
   def index = Action(Ok(views.html.index()))
 
   // grep words
   def grepDictionary(search: String) = Action {
-    Ok.stream(
-      Enumerator.fromFile(new File("/usr/share/dict/words")) &>
-      CLI.pipe(Seq("grep", search), 64)
-    ) withHeaders (CONTENT_TYPE -> "text/plain")
+    val dictionary = Enumerator.fromFile(new File("/usr/share/dict/words"))
+    Ok.stream(dictionary &> grep(search))
+      .withHeaders(CONTENT_TYPE -> "text/plain")
   }
+
 
   // Re-stream a web radio by adding echo with sox
   def webRadioWithEcho = Action {
     val src = "http://radio.hbr1.com:19800/ambient.ogg"
-    val stream = Concurrent.unicast[Array[Byte]]( channel => {
-      WS.url(src).get { header => Iteratee.foreach[Array[Byte]](channel.push(_)) }
-    }, () => ())
-    val addEcho = CLI.pipe("sox -t ogg - -t ogg - echo 0.5 0.7 60 1")
-    Ok.stream(stream &> addEcho).withHeaders(CONTENT_TYPE -> "audio/ogg")
+    Ok.stream((socket: Socket.Out[Array[Byte]]) => WS.url(src).get(headers => addEchoToOgg &> socket))
+      .withHeaders(CONTENT_TYPE -> "audio/ogg")
   }
 
   // Retrieve an online video, resize it, stream it
   def downloadReEncodeAndStreamVideo = Action {
-    //val src = "http://ftp.nluug.nl/pub/graphics/blender/demo/movies/glsl_bird.avi"
     val src = "http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.1080p.mkv"
-    val stream = Concurrent.unicast[Array[Byte]]( channel => {
-      WS.url(src).get { header => Iteratee.foreach[Array[Byte]](channel.push(_)) }
-    }, () => ())
-    val scaleHalf = CLI.pipe("ffmpeg -i pipe:0 -vf scale=iw/2:-1 -f avi pipe:1")
-    Ok.stream(stream &> scaleHalf).withHeaders(CONTENT_TYPE -> "video/avi")
+    Ok.stream((socket: Socket.Out[Array[Byte]]) => WS.url(src).get(headers => scaleVideoHalf &> socket))
+      .withHeaders(CONTENT_TYPE -> "video/avi")
   }
   
   // Use a local video, resize it, stream it
   def reEncodeAndStreamVideo = Action {
-    // download the video from: http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.1080p.mkv
-    val stream = Enumerator.fromFile(Play.getFile("Sintel.2010.1080p.mkv"))
-    val scaleHalf = CLI.pipe("ffmpeg -i pipe:0 -vf scale=iw/2:-1 -f avi pipe:1")
-    Ok.stream(stream &> scaleHalf).withHeaders(CONTENT_TYPE -> "video/avi")
-  }
-
-
-
-  // List all files in this Play project
-  def find = Action {
-    Ok.stream(CLI.enumerate("find .") >>> Enumerator.eof)
-      .withHeaders(
-        CONTENT_TYPE -> "text/plain"
-      )
-  }
-  
-  // Retrieve a webpage and display it (of course, this is just for the demo, I won't use curl, prefer using WS)
-  def curl = Action {
-    Ok.stream(CLI.enumerate(Process("curl -s http://blog.greweb.fr/")) >>> Enumerator.eof)
-      .withHeaders(
-        CONTENT_TYPE -> "text/html"
-      )
+    val stream = Enumerator.fromFile(Play.getFile("Sintel.2010.1080p.mkv")) // download it on sintel.org
+    Ok.stream(stream &> scaleVideoHalf)
+      .withHeaders(CONTENT_TYPE -> "video/avi")
   }
 
   // consume a ogg sound, add an echo effect and store in a /tmp/out.ogg file
   def audioEchoEffectGenerate = Action {
     val file = File.createTempFile("sample_with_echo_", ".ogg") // handle myself the output
     val enum = Enumerator.fromFile(Play.getFile("conf/exemple.ogg"))
-    val result = CLI.consume(Process("sox -t ogg - -t ogg - echo 0.5 0.7 60 1") #> file)(enum)
+    val addEchoToLocalOgg = CLI.consume(Process("sox -t ogg - -t ogg - echo 0.5 0.7 60 1") #> file)(_)
     AsyncResult {
-      result.map { _ =>
+      addEchoToLocalOgg(enum) map { _ =>
         Ok("'"+file.getAbsolutePath+"' file has been generated.")
       }
     }
+  }
+
+  // List all files in this Play project
+  def find = Action {
+    Ok.stream(CLI.enumerate("find .") >>> Enumerator.eof)
+      .withHeaders(CONTENT_TYPE -> "text/plain")
+  }
+  
+  // Retrieve a webpage and display it (of course, this is just for the demo, I won't use curl, prefer using WS)
+  def curl = Action {
+    Ok.stream(CLI.enumerate("curl -s http://blog.greweb.fr/") >>> Enumerator.eof)
+      .withHeaders(CONTENT_TYPE -> "text/html")
   }
   
 }
